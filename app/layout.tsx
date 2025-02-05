76ddc92d8089765e367421bd6d6be4b97be86c6a
菊池駿 <awiiin@kikuchishunnoMac-Studio.local>
2025-02-06 00:50:28 +0900

    2ndcommit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Bee AIカスタマイズされた最先端の顧客対応',
+  description: 'AIカスタマイズされた最先端の顧客対応 時間・ストレス・生産性の解放、ここに極まる。AI駆動のチャット返信、為替連動価格調整、税務管理まで。 Beeが全てを一元化し、小さい時間の解放がチームの成果を最大化させます'
 }
 
 export default function RootLayout({
