faf6a0c86aa1a7e2bff8a962940ac323b0ba6cd1
菊池駿 <awiiin@kikuchishunnoMac-Studio.local>
2025-03-25 08:20:18 +0900

    Bee お問い合わせ改善

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -91,7 +91,7 @@ export default function Contact() {
           </div>
           
           {/* 右側: お問い合わせフォーム */}
-          <div className="lg:col-span-3">
+          {/* <div className="lg:col-span-3">
             <div className="bg-white p-8 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 border border-orange-200">
               <h3 className="text-2xl font-bold text-orange-700 mb-6">お問い合わせフォーム</h3>
               
@@ -203,7 +203,7 @@ export default function Contact() {
                 </div>
               </form>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
